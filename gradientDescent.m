287cca19f148ed1a9c0650b3858b4f2a744c8eb9
willowcheng <e.wingus@Gmail.com>
2016-03-20 19:48:23 -0400

    Gradient descent calculation

--- gradientDescent.m
+++ gradientDescent.m
@@ -16,10 +16,14 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+    
+    theta_change = theta;
+    
+    for index = 1:length(theta)
+        theta_change(index) = alpha * (1/m) * sum((X * theta - y) .* X(:,index));
+    end
 
-
-
-
+    theta = theta - theta_change;
 
 
 
